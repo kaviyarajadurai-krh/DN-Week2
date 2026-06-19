9fd101950f5c6a42c67040ca1c5dcdfe5b554a9d
kaviyarajadurai-krh <kaviyarajadurai@gmail.com>
2026-06-19 23:03:32 +0530

    Delete Exercise 8-StrategyPatternExample directory

--- Exercise 8-StrategyPatternExample/src/CreditCardPayment.java
+++
@@ -1,7 +0,0 @@
-public class CreditCardPayment implements PaymentStrategy {
-
-    @Override
-    public void pay(int amount) {
-        System.out.println("Paid " + amount + " using Credit Card");
-    }
-}
--- Exercise 8-StrategyPatternExample/src/PayPalPayment.java
+++
@@ -1,7 +0,0 @@
-public class PayPalPayment implements PaymentStrategy {
-
-    @Override
-    public void pay(int amount) {
-        System.out.println("Paid " + amount + " using PayPal");
-    }
-}
